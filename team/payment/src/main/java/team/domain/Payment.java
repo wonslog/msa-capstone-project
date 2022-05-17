7b401594ad9ccb2b6bf9b72c112443a95d745d52
jinsong cho <wlsthd3322@naver.com>
2022-05-17 07:00:14 +0000

    circuit test

--- team/payment/src/main/java/team/domain/Payment.java
+++ team/payment/src/main/java/team/domain/Payment.java
@@ -32,9 +32,22 @@ public class Payment  {
 
     @PostPersist
     public void onPostPersist(){
+
         PaymentApproved paymentApproved = new PaymentApproved();
         BeanUtils.copyProperties(this, paymentApproved);
         paymentApproved.publishAfterCommit();
+
+    }
+
+    @PrePersist
+    private void paymentCheck(){
+        try {
+        Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+        if( "".equals(this.payStatus) || this.payStatus == null ){
+        }
     }
 
     @PreRemove
